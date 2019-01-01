977fbf2d2a6c9a3422a19f7111cf8d70bb5c59b6
Noliani <noliani.clemente@gmail.com>
2019-01-01 09:55:41 -0500

    removed wrong alert

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,6 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <transition name="grow">
-                    <div v-if="showWrongAlert" class="alert alert-danger text-center text-danger">
-                        <span>Wrong answer!</span>
-                        <button
-                            class="btn btn-primary" 
-                            @click="showWrongAlert = false">
-                            Try Again
-                        </button>
-                    </div>
-                </transition>
                 <transition :name="answerTransition" mode="out-in">
                     <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
                 </transition>
