3aba70ffac337e30f5b94726869b6a6d6a602a36
Noliani <noliani.clemente@gmail.com>
2019-01-01 09:54:12 -0500

    added transitions

--- src/App.vue
+++ src/App.vue
@@ -8,37 +8,82 @@
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-            
+                <transition name="grow">
+                    <div v-if="showWrongAlert" class="alert alert-danger text-center text-danger">
+                        <span>Wrong answer!</span>
+                        <button
+                            class="btn btn-primary" 
+                            @click="showWrongAlert = false">
+                            Try Again
+                        </button>
+                    </div>
+                </transition>
+                <transition :name="answerTransition" mode="out-in">
                     <component :is="mode" @answered="answered($event)" @confirmed="mode = 'app-question'"></component>
-        
+                </transition>
             </div>
         </div>
     </div>
 </template>
 
+<style>
+    /* FADE */
+    .fade-enter-active, .fade-leave-active {
+    transition: opacity .1s;
+    }
+    .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+    }
+
+    /* BOUNCE */
+    .bounce-enter-active {
+    animation: bounce-in .5s;
+    }
+    .bounce-leave-active {
+    animation: bounce-in .5s reverse;
+    }
+    @keyframes bounce-in {
+        0% {
+            transform: scale(0);
+        }
+        50% {
+            transform: scale(1.5);
+        }
+        100% {
+            transform: scale(1);
+        }
+    }
+</style>
+
 <script>
     import Question from './components/Question.vue';
     import Answer from './components/Answer.vue';
+    import Wrong from './components/Wrong.vue';
 
     export default {
         data() {
             return {
-                mode: 'app-question'
+                mode: 'app-question',
+                answerTransition: 'bounce',
+                showWrongAlert: false,
             }
         },
         methods: {
           answered(isCorrect) {
               if (isCorrect) {
+                  this.answerTransition = 'bounce';
                   this.mode = 'app-answer';
               } else {
-                  this.mode = 'app-question';
-                  alert('Wrong, try again!');
+                  this.answerTransition = 'fade';
+                  this.mode = 'app-wrong';
+                //   alert('Wrong, try again!');
               }
           }
         },
         components: {
             appQuestion: Question,
-            appAnswer: Answer
+            appAnswer: Answer,
+            appWrong: Wrong,
         }
     }
 </script>
---
+++ src/components/Wrong.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="alert alert-danger text-center">
+        <h1>Sorry, wrong answer!</h1>
+        <hr>
+        <button class="btn btn-primary" @click="onTryAgain">Try a New One</button>
+    </div>
+</template>
+<style>
+
+</style>
+<script>
+
+    export default{
+        methods: {
+            onTryAgain() {
+                this.$emit('confirmed');
+            }
+        }
+    }
+</script>
